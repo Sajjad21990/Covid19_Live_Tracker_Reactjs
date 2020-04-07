43e78ba28500983c359db2ee4643077027f48e10
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-04-07 09:07:59 +0530

    Covid-19 Live Tracker

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends React.Component {
         <Paper elevation={3}>
           <Typography>Created By: Sajjad Haider Sayed</Typography>
         </Paper>
-        {/* "homepage": "http://Sajjad21990.github.io/Covid-19-Live-Tracker-App-using-Reactjs",
+        {/* 
         "predeploy": "npm run build",
     "deploy": "gh-pages -d build", */}
       </div>
